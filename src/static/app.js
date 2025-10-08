02dc3c765c42378e20fb52002d56c42269d2b5e9
Jayank Tyagi <87943225+jayanktyagi@users.noreply.github.com>
2025-10-08 11:56:57 +0000

    Clear activity dropdown on fetch and add styles for participants section

--- src/static/app.js
+++ src/static/app.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // Clear loading message
       activitiesList.innerHTML = "";
+      activitySelect.innerHTML = '<option value="">-- Select an activity --</option>'; // Clear dropdown
 
       // Populate activities list
       Object.entries(activities).forEach(([name, details]) => {
